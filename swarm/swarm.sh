2117f4211487a74ea9d02134de256d25e5537f4b
Dylan Groff <dylan.mgroff@gmail.com>
2025-11-25 16:29:58 -0500

    Update swarm.sh

--- swarm/swarm.sh
+++ swarm/swarm.sh
@@ -150,16 +150,3 @@ for newnode in "${all[@]}"; do
 EOF
   echo -e " \033[32;5m$newnode - GlusterFS mounted on reboot\033[0m"
 done
-
-# OPTIONAL #
-# Step 7: Add Portainer
-ssh -tt $user@$manager1 -i ~/.ssh/$certName sudo su <<EOF
-mkdir /mnt/Portainer
-curl -L https://raw.githubusercontent.com/JamesTurland/JimsGarage/main/Docker-Swarm/portainer-agent-stack.yml -o portainer-agent-stack.yml
-docker stack deploy -c portainer-agent-stack.yml portainer
-docker node ls
-docker service ls
-gluster pool list
-exit
-EOF
-echo -e " \033[32;5mPortainer deployed\033[0m"
